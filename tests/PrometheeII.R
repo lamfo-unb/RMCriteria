4a8f5de2ba727876d5a791552c13f8b2729d73ca
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-11-30 15:25:54 -0500

    Promethee II - Example

---
+++ tests/PrometheeII.R
@@ -0,0 +1,8 @@
+dados<-matrix(c(5.2,-3.5,
+                4.3,-1.2,
+                6.7,-2.0),byrow = T, ncol=2,nrow=3)
+
+parms<-matrix(c(NA,
+                NA),byrow=TRUE,ncol=1,nrow=2)
+
+RMCriteria::PrometheeII(dados,c(0.3,0.7),c(0,0),parms,FALSE)
