e853cfbc6168ded2091935e59752696b5356f24b
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-11-29 22:52:08 -0200

    Initial trial.

--- src/PrometheeII.cpp
+++ src/PrometheeII.cpp
@@ -5,33 +5,89 @@ using namespace Rcpp;
 
 
 //Error Measure
-double GaussianPreference(double delta, Eigen::VectorXd parms);
+void GaussianPreference(Eigen::MatrixXd &matDelta, double sigma);
 
 
 // Create the Kernel matrix
-// @param datMat  Matrix with the data
-// @param function Kernel Function
-// @param parms vector of parameters fot the kernel
-// @return Kernel Matrix
-Eigen::MatrixXd KernelMatrix(Eigen::MatrixXd datMat,const std::function<double (arma::vec, arma::vec, arma::vec)> kernel, arma::vec parms){
+// @param datVec  Column of the dataset
+// @param int Type od preference function
+// @return Preference Matrix
+Eigen::MatrixXd matPrometheeII(Eigen::VectorXd datVec,int prefFunction){
   //Get the number of rows
-  int rows=datMat.rows();
-  //Typecasting
-  arma::mat datMat2 = convertEigenToArma(datMat);
-  //Initialize the matriz
-  Eigen::MatrixXd matKernel = Eigen::MatrixXd::Zero(rows,rows);
-  for(unsigned int c1=0;c1<rows;c1++){
-    for(unsigned int c2=c1;c2<rows;c2++){
-      //First column with variables
-      arma::vec vec1 = datMat2.row(c1).t();
-      //Second column with variables
-      arma::vec vec2 = datMat2.row(c2).t();
-      //Calculate the kernel value
-      double val= kernel(vec1,vec2,parms);
-      //Store the kernel value
-      matKernel(c1,c2)=matKernel(c2,c1)=val;
+  int rows=datVec.size();
+
+  //Initialize the matPromethee
+  Eigen::MatrixXd matPromethee = Eigen::MatrixXd::Zero(rows, rows);
+
+  //Variance
+  double sum2 = 0;
+  double sum  = 0;
+  double cont = 0;
+  //Initialize the computation
+  for(int i=0;i<rows;i++){
+    for(int j=i+1;j<rows-1;j++){
+      //Compute the difference
+      double delta = datVec(i)-datVec(j);
+      if(delta>0){
+        matPromethee(i,j)=delta;
+      }
+      else{
+        matPromethee(j,i)=(1.0)*delta;
+      }
+      sum2=sum2+std::pow(matPromethee(j,i),2);
+      sum=sum+delta;
+      cont=cont+1;
     }
   }
 
-  return(matKernel);
+  //Calculate the sigma
+  double sigma = (sum2/cont)-(std::pow(sum/cont,2));
+
+  //Gaussian Preference
+  if(prefFunction==0){
+    GaussianPreference(matPromethee,sigma);
+  }
+  else if(prefFunction==1){
+  }
+
+  return(matPromethee);
 }
+
+
+// Create the Kernel matrix
+// @param datVec  Column of the dataset
+// @param int Type od preference function
+// @return Preference Matrix
+Eigen::VectorXd PrometheeII(Eigen::MatrixXd datMat, Eigen::VectorXd vecWeights, Eigen::VectorXi prefFunction){
+  //Get the number of rows
+  int rows=datMat.rows();
+
+  //Get the number of columns
+  int cols=datMat.cols();
+
+  //Initialize the matPromethee
+  Eigen::MatrixXd matPromethee = Eigen::MatrixXd::Zero(rows, rows);
+
+  //For each column
+  for(int col=0;col<cols;col++){
+    if(vecWeights(col)>0){
+      //Create the delta matrix
+      Eigen::MatrixXd matTemp  = matPrometheeII(datMat.col(col), prefFunction(col));
+      //Multiply the weight
+      matTemp = matTemp*vecWeights(col);
+      //Accumulate the matrix
+      matPromethee = matPromethee+matTemp;
+    }
+  }
+
+  //Normalize the matrix
+  matPromethee = matPromethee/vecWeights.sum();
+
+  //Create the Flow matrix
+  Eigen::MatrixXd matFlow = Eigen::MatrixXd::Zero(cols, cols);
+  for(int col=0;col<cols;col++){
+    matFlow.row(col) = matPromethee.row(col).sum();
+  }
+
+}
+
