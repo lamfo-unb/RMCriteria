6f65acc942ba18acb709b8f6c5a8a48b3cd3bbc0
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-11-30 15:26:41 -0500

    Create new preference functions.

--- src/Preference.cpp
+++ src/Preference.cpp
@@ -2,12 +2,100 @@
 // [[Rcpp::depends(RcppEigen)]]
 using namespace Rcpp;
 
-//Mean Square Error
+
+//Usual Preference Function
+void UsualPreference(Eigen::MatrixXd &matDelta){
+  int rows = matDelta.rows();
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      if(matDelta(i,j)<=0){
+        matDelta(i,j) = 0.0;
+      }
+      else{
+        matDelta(i,j) = 1.0;
+      }
+
+    }
+  }
+}
+
+
+//UShape Preference Function
+void UShapePreference(Eigen::MatrixXd &matDelta, double q){
+  int rows = matDelta.rows();
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      if(matDelta(i,j)<=q){
+        matDelta(i,j) = 0.0;
+      }
+      else{
+        matDelta(i,j) = 1.0;
+      }
+
+    }
+  }
+}
+
+//VShape Preference Function
+void VShapePreference(Eigen::MatrixXd &matDelta, double p){
+  int rows = matDelta.rows();
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      if(matDelta(i,j)<=0){
+        matDelta(i,j) = 0.0;
+      }
+      else if(matDelta(i,j)<=p){
+        matDelta(i,j) = matDelta(i,j)/p;
+      }
+      else{
+        matDelta(i,j) = 1.0;
+      }
+    }
+  }
+}
+
+//Level Preference Function
+void LevelPreference(Eigen::MatrixXd &matDelta,double q, double p){
+  int rows = matDelta.rows();
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      if(matDelta(i,j)<=q){
+        matDelta(i,j) = 0.0;
+      }
+      else if(matDelta(i,j)<=p){
+        matDelta(i,j) = 0.5;
+      }
+      else{
+        matDelta(i,j) = 1.0;
+      }
+    }
+  }
+}
+
+//Level Preference Function
+void VShapeIndPreference(Eigen::MatrixXd &matDelta, double q, double p){
+  int rows = matDelta.rows();
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      if(matDelta(i,j)<=q){
+        matDelta(i,j) = 0.0;
+      }
+      else if(matDelta(i,j)<=p){
+        matDelta(i,j) = (matDelta(i,j)-q)/(p-q);
+      }
+      else{
+        matDelta(i,j) = 1.0;
+      }
+    }
+  }
+}
+
+//Gaussian Preference Function
 void GaussianPreference(Eigen::MatrixXd &matDelta, double sigma){
   int rows = matDelta.rows();
   for(int i=0;i<rows;i++){
     for(int j=0;j<rows;j++){
-      if(matDelta(i,j)<0){
+      if(matDelta(i,j)<=0){
         matDelta(i,j) = 0.0;
       }
       else{
--- src/Preference.h
+++ src/Preference.h
@@ -2,7 +2,19 @@
 #ifndef PREFERENCE_H
 #define PREFERENCE_H
 
+//Gaussian Preference Function
 void GaussianPreference(Eigen::MatrixXd &matDelta, double sigma);
+//Usual Preference Function
+void UsualPreference(Eigen::MatrixXd &matDelta);
+//UShape Preference Function
+void UShapePreference(Eigen::MatrixXd &matDelta, double q);
+//VShape Preference Function
+void VShapePreference(Eigen::MatrixXd &matDelta, double p);
+//Level Preference Function
+void LevelPreference(Eigen::MatrixXd &matDelta,double q, double p);
+//Level Preference Function
+void VShapeIndPreference(Eigen::MatrixXd &matDelta, double q, double p);
+
 
 
 // This is the end of the header guard
