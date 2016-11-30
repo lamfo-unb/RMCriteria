a5501e894dcc540d20324e831afb0a3272054c4c
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-11-30 08:51:26 -0500

    Correct Gaussian Preference Function

--- src/Preference.cpp
+++ src/Preference.cpp
@@ -7,7 +7,13 @@ void GaussianPreference(Eigen::MatrixXd &matDelta, double sigma){
   int rows = matDelta.rows();
   for(int i=0;i<rows;i++){
     for(int j=0;j<rows;j++){
-      matDelta(i,j) = 1-std::exp(-1.0*std::pow(matDelta(i,j),2.0))
+      if(matDelta(i,j)<0){
+        matDelta(i,j) = 0.0;
+      }
+      else{
+        matDelta(i,j) = 1-std::exp((-1.0*std::pow(matDelta(i,j),2.0))/(2.0*sigma));
+      }
+
     }
   }
 }
