e8f84b8bc66e2e9732e34b51826f3c131e377cbc
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-12-07 09:13:04 -0500

    Automatic bandwidth selection

--- tests/PrometheeIVKernel.R
+++ tests/PrometheeIVKernel.R
@@ -2,11 +2,7 @@ dados<-matrix(c(5.2,-3.5,
                 4.3,-1.2,
                 6.7,-2.0),byrow = T, ncol=2,nrow=3)
 
-parms<-matrix(c(1.0,
-                1.3),byrow=TRUE,ncol=1,nrow=2)
-
-bands<-matrix(c(0.5,
-                2.0),byrow=TRUE,ncol=1,nrow=2)
+bands<-as.matrix(apply(dados,2,bw.nrd0))
 
 RMCriteria::PrometheeIVKernel(dados,c(0.3,0.7),c(0,0),parms, bands, FALSE)
 
