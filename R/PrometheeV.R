b6a65f8fcb9c5fe1da6fff6e7032cf9f63f7dadb
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-12-07 10:09:15 -0500

    Promethee V - Function and Examples.

--- R/PrometheeV.R
+++ R/PrometheeV.R
@@ -1,8 +1,8 @@
-PrometheeV<-function(datMat,vecWeights,prefFunction,parms, bounds,normalize){
+PrometheeV<-function(datMat, vecWeights, prefFunction, parms, bounds, normalize){
   #Execute the Promethee II
-  f.obj <- RMCriteria::PrometheeII(datMat,vecWeights,prefFunction,normalize)
-  f.dir <- rep("<=", nrow(datMat))
-  f.con <- t(matrix(datMat))
+  f.obj <- RMCriteria::PrometheeII(datMat, vecWeights, prefFunction, parms, normalize)
+  f.dir <- rep("<=", ncol(datMat))
+  f.con <- t(datMat)
   f.rhs <- bounds
   return(lpSolve::lp("max", f.obj, f.con, f.dir, f.rhs, all.bin=TRUE))
 }
---
+++ tests/PrometheeV.R
@@ -0,0 +1,10 @@
+dados<-matrix(c(5.2,-3.5,
+                4.3,-1.2,
+                6.7,-2.0),byrow = T, ncol=2,nrow=3)
+
+parms<-matrix(c(NA,
+                NA),byrow=TRUE,ncol=1,nrow=2)
+
+RMCriteria::PrometheeV(dados,c(0.3,0.7),c(0,0),parms,c(7,-1), FALSE)
+
+
