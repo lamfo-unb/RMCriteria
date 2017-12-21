680e70648a2c53a9781c35303a449dbfe29517cc
Pedro <pedro.melo.albuquerque@gmail.com>
2017-12-21 13:03:45 -0200

    Begin the Promethee Network script. Issue #2

---
+++ tests/PrometheeNetwork.R
@@ -0,0 +1,13 @@
+dados<-matrix(c(5.2,-3.5,
+                4.3,-1.2,
+                6.7,-2.0),byrow = T, ncol=2,nrow=3)
+
+parms<-matrix(c(NA,
+                NA),byrow=TRUE,ncol=1,nrow=2)
+
+#RMCriteria::PrometheeII(dados,c(0.3,0.7),c(0,0),parms,FALSE)
+
+#Step 1: Construct the RPrometheeArguments
+PromObj <- RPrometheeConstructor(datMat=dados,vecWeights=c(0.3,0.7),vecMaximiz=c(F,T),prefFunction=c(0,0),parms=parms,normalize=FALSE)
+res <- RPrometheeII(PromObj)
+str(res)
