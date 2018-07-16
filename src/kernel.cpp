45cdb95dc825d129eeeb7dc35fa771e0c8d0f5bc
Gustavo Monteiro <monteirogustavop@gmail.com>
2018-07-16 18:09:05 -0300

    Add more preference functions for Kernel Promethee.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -4,7 +4,7 @@
 #include <RcppNumerical.h>
 using namespace Numer;
 
-// Finalizar funções de preferência, pensar no PhiMinus e encapsulamento no R (função S4 que chame o Rcpp)
+// Finalizar fun??es de prefer?ncia, pensar no PhiMinus e encapsulamento no R (fun??o S4 que chame o Rcpp)
 
 // [[Rcpp::export]]
 double GaussianPrefKernel(double y,Eigen::VectorXd vec, double band, double sigma, bool plus) {
@@ -59,7 +59,7 @@ double UsualPrefKernel(double y,Eigen::VectorXd vec, double band, bool plus) {
 
 
 // [[Rcpp::export]]
-double VShapePrefKernel(double y,Eigen::VectorXd vec, double band, bool plus, double q) {
+double UShapePrefKernel(double y,Eigen::VectorXd vec, double band, bool plus, double q) {
   //Calculate kernel
   const double pinum = 3.14159265359;
   const double pi2 = 1.0/std::sqrt(2*pinum);
@@ -85,7 +85,7 @@ double VShapePrefKernel(double y,Eigen::VectorXd vec, double band, bool plus, do
 
 
 // [[Rcpp::export]]
-double LevelPreference(double y,Eigen::VectorXd vec, double band, bool plus, double q, double p) {
+double LevelPrefKernel(double y,Eigen::VectorXd vec, double band, bool plus, double q, double p) {
   //Calculate kernel
   const double pinum = 3.14159265359;
   const double pi2 = 1.0/std::sqrt(2*pinum);
@@ -113,6 +113,40 @@ double LevelPreference(double y,Eigen::VectorXd vec, double band, bool plus, dou
   return(res);
 }
 
+// [[Rcpp::export]]
+double VShapePrefKernel(double y,Eigen::VectorXd vec, double band, bool plus, double p) {
+  //Calculate kernel
+  const double pinum = 3.14159265359;
+  const double pi2 = 1.0/std::sqrt(2*pinum);
+  if(plus == false) vec = -vec;
+  // Kernel Function
+  double K = (-(vec.array()-y).square().array()/(2*band*band)).exp().sum();
+  K = K * pi2;
+  K = (1/(vec.size()*band)) * K;
+
+  //  Preference function
+  double res = 0.0;
+  for(int j = 0.0; j < vec.size(); j++){
+    for(int i = 0.0; i < vec.size(); i++){
+      double deltaji = vec(j) - vec(i);
+      if(deltaji <= 0.0){
+        double qq = 0.0;
+        res = res + (qq * K);
+      }
+      else if(deltaji <= p){
+        double qq = deltaji/p;
+        res = res + (qq * K);
+      } else{
+        double qq = 1.0;
+        res = res + (qq * K);
+      }
+    }
+  }
+  return(res);
+}
+
+
+
 
 
 /*** R
@@ -124,7 +158,7 @@ band <- 0.5^2
 point <- 2
 sigma <- 0.7^2
 q <- 0.4
-p <- 1
+p <- 2
 
 
 K <- (1/(length(vec)*band)) * (1/(sqrt(2*pi))) * sum(exp(-(vec-2)^2/(2*band*band)))
@@ -162,7 +196,7 @@ paste("Usual Rcpp", UsualPrefKernel(point, vec, band, TRUE))
 paste("Usual R", res)
 
 ######################
-# V Shape Preference
+# U Shape Preference
 
 res <- 0
 for(j in 1:length(vec)){
@@ -175,8 +209,8 @@ for(j in 1:length(vec)){
   }
 }
 
-paste("VShape Rcpp: ", VShapePrefKernel(point, vec, band, TRUE, q))
-paste("VShape R", res)
+paste("UShape Rcpp: ", UShapePrefKernel(point, vec, band, TRUE, q))
+paste("UShape R", res)
 
 ######################
 # Level Preference
@@ -196,7 +230,33 @@ for(j in 1:length(vec)){
   }
 }
 
-paste("Level Rcpp: ", LevelPreference(point, vec, band, TRUE, q, p))
+paste("Level Rcpp: ", LevelPrefKernel(point, vec, band, TRUE, q, p))
 paste("Level R", res)
 
+
+######################
+# V Shape Pref Preference
+
+res <- 0
+for(j in 1:length(vec)){
+  for(i in 1:length(vec)){
+    deltaji <- vec[j] - vec[i]
+    if(deltaji <= 0){
+      qq <- 0.0 * K
+      res <- res + qq
+    }
+    else if(deltaji <= p){
+      qq <- deltaji/p
+      res <- res + qq
+    }
+    else {
+      qq <- 1 * K
+      res <- res + qq
+    }
+  }
+}
+
+
+paste("V Shape Rcpp: ", VShapePrefKernel(point, vec, band, TRUE, p))
+paste("V Shape R", res)
 */
