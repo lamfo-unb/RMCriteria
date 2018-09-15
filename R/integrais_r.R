8f44aeb5862f94e41a47e662660544285b342dc3
monteirogustavo <monteirogustavop@gmail.com>
2018-09-14 22:47:56 -0300

    Update integrals in R.

--- R/integrais_r.R
+++ R/integrais_r.R
@@ -1,45 +1,51 @@
 alt <- matrix(c(5.2, 4.3, 6.7,
                 1.3, 2.5, 4.0), ncol = 2, nrow = 3)
 
-alt <- matrix(c(rnorm(4000)), ncol = 2, nrow = 2000)
+# alt <- matrix(c(rnorm(4000)), ncol = 2, nrow = 2000)
+#
+# b <- c(0.25, 0.25)
+#
+# usualpref1 <- function(x, j, k){
+#   ifelse(alt[j, k] - x >= 0, 1, 0)
+# }
+#
+# usualpref2 <- function(x, k){
+#   ifelse(alt[, k] - x >= 0, 1, 0)
+# }
+#
+# x <- 2
+#
+# integrand <- function(x){
+#   (1/sqrt(2*pi))*sum(exp(-0.5*((usualpref1(x, 1, 1) - usualpref2(x, 1))/b[k])^2))*usualpref1(x, 1, 1)
+# }
+#
+# integrate(integrand, 0, 1)$value
+#####################################
 
-b <- c(0.25, 0.25)
+datMat <- matrix(c(5.2, 4.3, 6.7,
+                1.3, 2.5, 4.0), ncol = 2, nrow = 3)
+band <- c(0.25, 0.1)
+k <- 1
+#y <- NULL
 
-usualpref1 <- function(x, j, k){
-  ifelse(alt[j, k] - x >= 0, 1, 0)
-}
-
-usualpref2 <- function(x, k){
-  ifelse(alt[, k] - x >= 0, 1, 0)
-}
-
-x <- 2
-
-integrand <- function(x){
-  (1/sqrt(2*pi))*sum(exp(-0.5*((usualpref1(x, 1, 1) - usualpref2(x, 1))/b[k])^2))*usualpref1(x, 1, 1)
-}
-
-integrate(integrand, 0, 1)$value
-
-x <- 0
-k <- 2
-z <- data.frame(y)[1,]
+#x <- 0
 usualpref <- function(x,w){
   ifelse(w - x >= 0, 1, 0)
 }
 
-for(k in 1:ncol(alt)){
-  y<-alt[,k]
+for(k in 1:ncol(datMat)){
+    z <- data.frame(datMat)[1, k]
+    y<-datMat[,k]
 
-  res<-apply(data.frame(y),1,function(z){
+    res<-apply(data.frame(y), 1, function(z){
 
 
     integrand <- function(x){
-      (1/sqrt(2*pi))*sum(exp(-0.5*((usualpref(x,z) - usualpref(x,y))/b[k])^2))*usualpref(x,z)
+      (1/sqrt(2*pi))*sum(exp(-0.5*((usualpref(x,z) - usualpref(x,y))/band[k])^2))*usualpref(x,z)
     }
 
-    integrate(integrand, 0, 1)$value
-  })
+    res[i, k] <- integrate(integrand, 0, 1)$value
+    #  })
 
-}
-
+  }
+)}
